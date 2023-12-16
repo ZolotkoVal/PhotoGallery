f489c64f1c5e5d6a5849fb9d9247a395a57bd5dc
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-16 18:29:26 +0700

    Десериализация JSON-текста в объекты модели

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -9,5 +9,5 @@ interface FlickrApi {
                 "&format=json" +
                 "&nojsoncallback=1" +
                 "&extras=url_s")
-    fun fetchPhotos(): Call<String>
+    fun fetchPhotos(): Call<FlickrResponse>
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -24,10 +24,11 @@ class PhotoGalleryFragment<View> : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchPhotos()
+        val flickrLiveData: LiveData<List<GalleryItem>> =
+            FlickrFetchr().fetchPhotos()
         flickrLiveData.observe(
             this,
-            Observer { responseString -> Log.d(TAG, "Response received:$responseString")
+            Observer { galleryItems -> Log.d(TAG, "Response received:$galleryItems")
             })
     }
     override fun onCreateView(
