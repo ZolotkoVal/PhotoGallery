db25da128fa86f5cf8bda7e87103f1370c4a379a
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-16 17:10:17 +0700

    Получение JSON-данных от Flickr

--- app/src/main/java/api/FlickrApi.kt
+++ app/src/main/java/api/FlickrApi.kt
@@ -4,6 +4,10 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+        "&api_key=6e6ecdb18423b677b17eaad3995add4e" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s")
+    fun fetchPhotos(): Call<String>
 }
