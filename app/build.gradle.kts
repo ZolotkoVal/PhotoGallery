ac1312be6abbddd5b78b12d4f09b91027190be72
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-19 09:58:37 +0700

    Добавьте в зависимости библиотеку Picasso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-scalars:2.5.0")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.4.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
