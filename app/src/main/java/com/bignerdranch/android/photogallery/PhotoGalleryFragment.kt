ea0232918938599d4e5d90c19656a5987938039a
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-19 09:59:12 +0700

    onCreateViewHolder onBindViewHolder

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
@@ -13,6 +14,7 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import api.FlickrApi
+import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -50,27 +52,36 @@ class PhotoGalleryFragment<View : Any> : Fragment() {
                 photoRecyclerView.adapter = PhotoAdapter(galleryItems)
             })
     }
-    private class PhotoHolder(itemTextView: TextView)
-        : RecyclerView.ViewHolder(itemTextView)
+    private class PhotoHolder(itemImageView: ImageView)
+        : RecyclerView.ViewHolder(itemImageView)
     {
-        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+        val bindImageView: (ImageView) = itemImageView
     }
-    private class PhotoAdapter(private val galleryItems: List<GalleryItem>)
+    private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>)
         : RecyclerView.Adapter<PhotoHolder>() {
+
         override fun onCreateViewHolder(
             parent: ViewGroup,
             viewType: Int
         ): PhotoHolder {
-            val textView =
-                TextView(parent.context)
-            return PhotoHolder(textView)
+            val view = layoutInflater.inflate(
+                R.layout.list_item_gallery,
+                parent,
+                false
+            ) as ImageView
+            return PhotoHolder(view)
         }
+
         override fun getItemCount(): Int = galleryItems.size
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
-            val galleryItem =
-                galleryItems[position]
-            holder.bindTitle(galleryItem.title)
-        } }
+            lateinit var itemImageView: ImageView
+            val galleryItem = galleryItems[position]
+            Picasso.get()
+                .load(galleryItem.url)
+                .placeholder(R.drawable.bill_up_close)
+                .into(holder.bindImageView)
+        }
+    }
 
     companion object {
         fun   newInstance()   = PhotoGalleryFragment<Any>()
