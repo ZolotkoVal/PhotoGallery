130c19d7975823214fbebd4d6964b2167ad69431
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-21 15:38:38 +0700

    изменения по 7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.squareup.retrofit2:retrofit:2.5.0")
