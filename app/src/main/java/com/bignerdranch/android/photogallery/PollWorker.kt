1d111ec0703ba1837335e78fe6ca7fd948f6b5ac
ZolotkoVal <valeriya.zolotarenko11@gmail.com>
2023-12-24 22:48:25 +0700

    Получение последних фотографий

--- app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollWorker.kt
@@ -10,7 +10,21 @@ private const val TAG = "PollWorker"
 class PollWorker(val context: Context, workerParams: WorkerParameters)
     : Worker(context, workerParams) {
     override fun doWork(): Result {
-        Log.i(TAG, "Work request triggered")
+        val query = QueryPreferences.getStoredQuery(context)
+        val lastResultId = QueryPreferences.getLastResultId(context)
+        val items: List<GalleryItem> = if (query.isEmpty()) {
+            FlickrFetchr().fetchPhotosRequest()
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } else {
+            FlickrFetchr().searchPhotosRequest(query)
+                .execute()
+                .body()
+                ?.photos
+                ?.galleryItems
+        } ?: emptyList()
         return Result.success()
     }
 }
